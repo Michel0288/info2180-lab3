b3418b7f3bb8bfcdd7dcd7e6b374d74b4abcb160
Michel Davies <micheldavies0288@gmail.com>
2020-10-10 00:44:52 -0500

    Added exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,31 @@
-onload=function(){ 
-    var c = document.getElementById("board").children.length; 
-    var i; 
-    for (i = 0; i <= (c-1); i++) 
-    { 
-        document.getElementById('board').getElementsByTagName('div')[i].setAttribute('class', 'square'); 
-    }
+onload=function(){
+    const player1='X';
+    const player2='O';
+    var array=[];
+    let currentplayer;
+    var getsquares=document.getElementById('board').getElementsByTagName('div');
+    var len=getsquares.length;
+    for(let i=0;i<len;i++){
+        getsquares[i].className+=('square');
+        getsquares[i].addEventListener('click',function(){
+            // console.log('Clicked')
+
+            getplayer()
+            function getplayer(){
+                
+                if(currentplayer===player1){
+                    currentplayer=player2;
+                    document.getElementById('board').getElementsByTagName('div')[i].className+=(' X');
+                }
+                else{
+                    currentplayer=player1;
+                    document.getElementById('board').getElementsByTagName('div')[i].className+=(' O');
+                }
+                array.push(getsquares[i].innerHTML=currentplayer);
+
+                // console.log(array)
+            }
+          
+        },{once:true});
+    }  
 }
